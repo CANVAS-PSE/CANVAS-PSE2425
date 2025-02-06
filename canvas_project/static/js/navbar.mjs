21241061282c19fc5236e6bcd75d2a7ed793a538
ucqov <ucqov@student.kit.edu>
2025-02-06 12:56:05 +0100

    open project functionality for navbar and additional keybindings

--- canvas_project/static/js/navbar.mjs
+++ canvas_project/static/js/navbar.mjs
@@ -44,40 +44,12 @@ export class Navbar {
 
     #setupFileOptions() {
         let createNewProject = document.getElementById('createNewProject');
-        let importButton = document.getElementById('import');
-        let exportButton = document.getElementById('export');
-
-        const fileInput = document.createElement('input');
-        fileInput.type = 'file';
-        // only allows h5 files
-        fileInput.accept = '.h5'; 
-        fileInput.style.display = 'none';
-        document.body.appendChild(fileInput);
 
         // ensure that the form is reset when the modal is closed
         createNewProject.addEventListener('hidden.bs.modal', function () {
             var form = createNewProject.querySelector('form');
             form.reset();
         });
-
-
-        importButton.onclick = (_) => {};
-
-        importButton.onclick = (_) => {
-            fileInput.click();
-        }
-
-        fileInput.onchange = (event) => {
-            const file = event.target.files[0];
-            if (file) {
-                console.log("Ausgewählte Datei:", file.name);
-                // TODO: open file as new project
-            }
-        }
-
-        exportButton.onclick = (_) => {
-        }
-
     }
 
     /**
