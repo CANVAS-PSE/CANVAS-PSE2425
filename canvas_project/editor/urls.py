c123461f778ac6f7b90600c95d674a394bda0526
plhrt <pl.hrtr@gmail.com>
2025-02-07 15:55:33 +0100

    fixed upload preview

--- canvas_project/editor/urls.py
+++ canvas_project/editor/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("<str:project_name>", views.editor, name="editor"),
     path("<str:project_name>/download", views.download, name="download"),
     path("<str:project_name>/renderHDF5", views.renderHDF5, name="renderHDF5"),
+    path("<str:project_name>/upload", views.uploadPreview, name="upload"),
 ]
