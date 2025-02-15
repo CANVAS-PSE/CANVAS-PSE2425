ee29a2f6cce356b769a9a13b6b35f44afeb9c399
plhrt <pl.hrtr@gmail.com>
2025-02-15 13:41:17 +0100

    added config for coverage.py and added some more tests for the editor view

--- canvas_project/editor/tests.py
+++ canvas_project/editor/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from project_management.models import Project
+from project_management.models import Project, Heliostat, Receiver, Lightsource
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.contrib.messages import get_messages
@@ -7,8 +7,26 @@ import os
 from django.conf import settings
 
 
-# TODO: Test the main editor view
 class EditorViewTest(TestCase):
+    """
+    Test suite for the EditorView.
+    This test suite includes tests for both GET and POST methods of the editor view.
+    It ensures that the editor view behaves correctly when accessed and when form data is submitted.
+
+    Methods
+    --------
+    setUp(self):
+        Set up the test environment by creating a user and a project.
+    test_get_method(self):
+        Test the GET method of the editor view to ensure it returns the correct status code and context data.
+    test_post_method(self):
+        Test the POST method of the editor view to ensure it handles form submissions correctly, including:
+        - Submitting a project with an existing name.
+        - Submitting a unique project without a file attached.
+        - Submitting a unique project with an HDF5 file attached.
+        - Submitting a project with an existing name and an HDF5 file attached.
+    """
+
     def setUp(self):
         # create new user
         user = User.objects.create_user(username="testuser", password="testpassword")
@@ -50,7 +68,9 @@ class EditorViewTest(TestCase):
             "description": "This is a unique test project",
         }
         response_unique = self.client.post(self.editor, data=form_data_unique)
-        self.assertEqual(response_unique.status_code, 302)
+        self.assertEqual(
+            response_unique.status_code, 302
+        )  # tests if the user gets redirected
         self.assertTrue(Project.objects.filter(name="uniqueProject").exists())
 
         # Create a unique project with a hdf5 file attached
@@ -64,7 +84,9 @@ class EditorViewTest(TestCase):
                 "file": hdf5_file,
             }
             response_with_file = self.client.post(self.editor, data=form_data_with_file)
-            self.assertEqual(response_with_file.status_code, 302)
+            self.assertEqual(
+                response_with_file.status_code, 302
+            )  # tests if the user gets redirected
             self.assertTrue(
                 Project.objects.filter(name="uniqueProjectWithFile").exists()
             )
@@ -93,8 +115,98 @@ class EditorViewTest(TestCase):
             )
 
 
-# TODO: Test the download view
+class DownloadViewTest(TestCase):
+    def setUp(self):
+        self.download = reverse("download", kwargs={"project_name": "testProject"})
+        user = User.objects.create_user(username="testuser", password="testpassword")
+        self.client = Client()
+        self.client.login(username="testuser", password="testpassword")
 
-# TODO: Test the render view
+        project = Project()
+        project.name = "testProject"
+        project.description = "This is a test project."
+        project.owner = user
+        project.save()
 
-# TODO: Test the preview view
+        # Add a heliostat to the project
+        heliostat = Heliostat()
+        heliostat.name = "testHeliostat"
+        heliostat.project = project
+        heliostat.save()
+
+        # Add a receiver to the project
+        receiver = Receiver()
+        receiver.name = "testReceiver"
+        receiver.project = project
+        receiver.save()
+
+        # Add a light source to the project
+        light_source = Lightsource()
+        light_source.name = "testLightSource"
+        light_source.project = project
+        light_source.save()
+
+    def test_download(self):
+        response = self.client.get(self.download)
+
+        # assert that response is a file response containing a hdf5 file
+        self.assertTrue(response.has_header("Content-Disposition"))
+        self.assertIn(
+            'attachment; filename="testProject.h5"', response["Content-Disposition"]
+        )
+
+        # TODO: Test if the file contains all the necassary information
+
+
+class RenderViewTest(TestCase):
+    def setUp(self):
+        self.render = reverse("renderHDF5", kwargs={"project_name": "testProject"})
+        user = User.objects.create_user(username="testuser", password="testpassword")
+        self.client = Client()
+        self.client.login(username="testuser", password="testpassword")
+
+        project = Project()
+        project.name = "testProject"
+        project.description = "This is a test project."
+        project.owner = user
+        project.save()
+
+    def test_render(self):
+        response = self.client.post(self.render)
+        self.assertEqual(response.status_code, 302)
+        # TODO: test if an hdf5 file was created
+
+    def test_wrong_method(self):
+        response = self.client.get(self.render)
+        self.assertEqual(response.status_code, 302)
+
+
+class PreviewViewTest(TestCase):
+    def setUp(self):
+        self.upload = reverse("upload", kwargs={"project_name": "testProject"})
+        user = User.objects.create_user(username="testuser", password="testpassword")
+        self.client = Client()
+        self.client.login(username="testuser", password="testpassword")
+
+        project = Project()
+        project.name = "testProject"
+        project.description = "This is a test project."
+        project.owner = user
+        project.save()
+
+    def test_upload(self):
+        project = Project.objects.get(name="testProject")
+
+        test_file_path = os.path.join(settings.BASE_DIR, "static/img/emptyEditor.png")
+        with open(test_file_path, "rb") as hdf5_file:
+            data = {
+                "preview": hdf5_file,
+            }
+            self.client.post(self.upload, data=data)
+        project = Project.objects.get(name="testProject")
+        # As the project has no file attribute assiciated with it when created, checking if it now exits tests the upload functionality
+        self.assertIsNotNone(project.preview.file)
+
+    def test_wrong_method(self):
+        response = self.client.get(self.upload)
+        self.assertEqual(response.status_code, 404)
