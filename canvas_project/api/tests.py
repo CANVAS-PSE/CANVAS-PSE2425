0fff55bb75c423ee5381a88668057ef7837cb837
plhrt <pl.hrtr@gmail.com>
2025-02-20 14:32:48 +0100

    finished the tests

--- canvas_project/api/tests.py
+++ canvas_project/api/tests.py
@@ -9,9 +9,8 @@ from project_management.models import (
     Lightsource,
     Settings,
 )
+from .serializers import ReceiverSerializer, HeliostatSerializer, LightsourceSerializer
 from django.contrib.auth.models import User
-
-# TODO: Update the get detail tests to include all parameters
 
 
 class APITestCase(TestCase):
@@ -28,7 +27,6 @@ class APITestCase(TestCase):
         data = {"name": "New Project"}
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Project.objects.count(), 2)
         self.assertEqual(Project.objects.get(id=response.data["id"]).owner, self.user)
 
     def test_get_projects(self):
@@ -64,13 +62,22 @@ class APITestCase(TestCase):
 
     def test_get_heliostat_detail(self):
         heliostat = Heliostat.objects.create(name="Heliostat 1", project=self.project)
+        heliostat.position_x = 42
+        heliostat.position_y = 42
+        heliostat.position_z = 42
+        heliostat.aimpoint_x = 42
+        heliostat.aimpoint_y = 42
+        heliostat.aimpoint_z = 42
+        heliostat.number_of_facets = 42
+        heliostat.kinematic_type = "test"
+        heliostat.save()
         url = reverse(
             "heliostat_detail",
             kwargs={"project_id": self.project.id, "pk": heliostat.id},
         )
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["name"], heliostat.name)
+        self.assertEqual(response.data, HeliostatSerializer(heliostat).data)
 
     def test_create_receiver(self):
         url = reverse("receiver_list", kwargs={"project_id": self.project.id})
@@ -92,12 +99,28 @@ class APITestCase(TestCase):
 
     def test_get_receiver_detail(self):
         receiver = Receiver.objects.create(name="Receiver 1", project=self.project)
+        receiver.position_x = 42
+        receiver.position_y = 42
+        receiver.position_z = 42
+        receiver.normal_x = 42
+        receiver.normal_y = 42
+        receiver.normal_z = 42
+        receiver.rotation_y = 42
+        receiver.receiver_type = "ideal"
+        receiver.curvature_e = 42
+        receiver.curvature_u = 42
+        receiver.plane_e = 42
+        receiver.plane_u = 42
+        receiver.resolution_e = 42
+        receiver.resolution_u = 42
+        receiver.save()
         url = reverse(
             "receiver_detail", kwargs={"project_id": self.project.id, "pk": receiver.id}
         )
         response = self.client.get(url, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["name"], receiver.name)
+        self.assertEqual(response.data, ReceiverSerializer(receiver).data)
 
     def test_create_lightsource(self):
         url = reverse("lightsource_list", kwargs={"project_id": self.project.id})
@@ -121,19 +144,27 @@ class APITestCase(TestCase):
         lightsource = Lightsource.objects.create(
             name="Lightsource 1", project=self.project
         )
+        lightsource.number_of_rays = 42
+        lightsource.lightsource_type = "test"
+        lightsource.distribution_type = "test"
+        lightsource.mean = 4.2
+        lightsource.covariance = 4.2
+        lightsource.save()
+
         url = reverse(
             "lightsource_detail",
             kwargs={"project_id": self.project.id, "pk": lightsource.id},
         )
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["name"], lightsource.name)
+        self.assertEqual(response.data, LightsourceSerializer(lightsource).data)
 
     def test_update_settings(self):
         settings = Settings.objects.get(project=self.project)
         url = reverse("settings_detail", kwargs={"project_id": self.project.id})
-        data = {"shadows": False}
+        data = {"shadows": False, "fog": False}
         response = self.client.put(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         settings.refresh_from_db()
         self.assertEqual(settings.shadows, False)
+        self.assertEqual(settings.fog, False)
