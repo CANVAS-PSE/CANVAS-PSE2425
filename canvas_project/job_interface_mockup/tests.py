a6f2eb3039c336f49ff98a5bb763f0fa91f20ec3
usygz <usygz@student.kit.edu>
2025-03-01 13:25:17 +0100

    model tests

--- canvas_project/job_interface_mockup/tests.py
+++ canvas_project/job_interface_mockup/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Job
+from project_management.models import Project
+from django.utils import timezone
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+
+    def test_job_owner_given(self):
+        job = Job.objects.create(owner=self.user)
+
+        self.assertTrue(isinstance(job, Job))
+        self.assertTrue((job.starting_time - timezone.now()).total_seconds() < 2)
+        self.assertEqual(job.owner, self.user)
+        self.assertIsNone(job.project)
+
+    def test_job_owner_and_project_given(self):
+        self.project = Project.objects.create(
+            name="Test project", description="Test project description", owner=self.user
+        )
+        job = Job.objects.create(owner=self.user, project=self.project)
+
+        self.assertTrue(isinstance(job, Job))
+        print((job.starting_time - timezone.now()).total_seconds)
+        self.assertTrue((job.starting_time - timezone.now()).total_seconds() < 2)
+        self.assertEqual(job.owner, self.user)
+        self.assertEqual(job.project, self.project)
