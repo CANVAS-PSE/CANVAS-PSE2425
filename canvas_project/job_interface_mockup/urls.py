75e8679a781c779c268624aa087f7316e26688b1
plhrt <pl.hrtr@gmail.com>
2025-06-14 12:25:19 +0200

    fixed tests

--- canvas_project/job_interface_mockup/urls.py
+++ canvas_project/job_interface_mockup/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path("<str:project_id>/", views.createNewJob, name="createNewJob"),
     path("<str:project_id>/<int:jobID>/", views.getJobStatus, name="jobStatus"),
-    path("<str:project_id>/hdf5", views.exampleCreateHDF5, name="exampleHDF5Creation"),
 ]
