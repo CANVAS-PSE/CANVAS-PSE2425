481c8c483193ba45e9a8e8b6ac983f018825b0da
plhrt <pl.hrtr@gmail.com>
2025-08-24 12:29:42 +0200

    switched editor to class based views

--- canvas_editor/editor/views.py
+++ canvas_editor/editor/views.py
@@ -1,12 +1,11 @@
 import os
 
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import FileResponse, HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
-from django.views.decorators.http import require_GET, require_POST
+from django.views import View
+from django.views.generic import TemplateView
 
 from hdf5_management.hdf5_manager import HDF5Manager
 from project_management.forms import ProjectForm
@@ -15,116 +14,69 @@ from project_management.models import (
 )
 
 
-@login_required
-@require_GET
-def editor(request, project_name):
-    """Handle the editor view for a given project.
+class EditorView(LoginRequiredMixin, TemplateView):
+    """Render the editor view."""
 
-    Parameters
-    ----------
-    request : HttpRequest
-        The request the user send to get here.
-    project_name : str
-        The project_name specified as a url parameter.
+    template_name = "editor/editor.html"
 
-    Returns
-    -------
-    HttpResponse
-        The editor page where the user can edit the project.
-    """
-    try:
-        project = Project.objects.get(owner=request.user, name=project_name)
-    except Project.DoesNotExist:
-        messages.error(request, "Project doesn't exist")
-        return redirect("projects")
+    def get_context_data(self, **kwargs):
+        """Get the context of the editor page."""
+        context = super().get_context_data(**kwargs)
+        project_name = self.kwargs.get("project_name")
+        request = self.request
+        project = get_object_or_404(Project, owner=request.user, name=project_name)
 
-    project.last_edited = timezone.now()
-    project.save()
+        project.last_edited = timezone.now()
+        project.save()
 
-    create_project_form = ProjectForm()
-    all_projects = Project.objects.filter(owner=request.user).order_by("-last_edited")
+        create_project_form = ProjectForm()
+        all_projects = Project.objects.filter(owner=request.user).order_by(
+            "-last_edited"
+        )
 
-    return render(
-        request,
-        "editor/editor.html",
-        context={
-            "project_id": project.pk,
-            "project_name": project.name,
-            "createProjectForm": create_project_form,
-            "projects": all_projects,
-        },
-    )
+        context.update(
+            {
+                "project_id": project.pk,
+                "project_name": project.name,
+                "createProjectForm": create_project_form,
+                "projects": all_projects,
+            }
+        )
+        return context
 
 
-def is_name_unique(user: User, project_name: str) -> bool:
-    unique = True
-    for project in user.projects.all():
-        if project.name == project_name:
-            unique = False
-            break
-    return unique
+class DownloadView(LoginRequiredMixin, View):
+    """Converts the specified project into an hdf5 file and downloads it."""
+
+    def get(self, request, project_name):
+        """Create and download the hdf5 file."""
+        project = get_object_or_404(Project, name=project_name, owner=request.user)
+
+        hdf5_manager = HDF5Manager()
+        hdf5_manager.create_hdf5_file(request.user, project)
+
+        # Set CANVAS_ROOT
+        path = f"./hdf5_management/scenarios/{request.user.id}_{project.name}ScenarioFile.h5"
+
+        response = FileResponse(
+            open(path, "rb"), as_attachment=True, filename=project_name + ".h5"
+        )
+
+        os.remove(path)
+
+        return response
 
 
-@login_required
-@require_GET
-def download(request, project_name):
-    """
-    Converts the specified project into an hdf5 file and downloads it.
+class UploadPreviewView(LoginRequiredMixin, View):
+    """Updates the preview of the project."""
 
-    Parameters
-    ----------
-    request : HttpRequest
-        The request the user send to get here.
-    project_name : str
-        The project_name specified as a url parameter.
+    def post(self, request, project_name):
+        """Upload the previed for this project."""
+        project = get_object_or_404(Project, name=project_name, owner=request.user)
+        file = request.FILES["preview"]
 
-    Returns
-    -------
-    HttpResponse
-        FileResponse to download the hdf5 file.
-    """
-    project = get_object_or_404(Project, name=project_name, owner=request.user)
+        project.preview.delete()
+        project.preview = file
+        project.save()
 
-    hdf5_manager = HDF5Manager()
-    hdf5_manager.create_hdf5_file(request.user, project)
-
-    # Set CANVAS_ROOT
-    path = (
-        f"./hdf5_management/scenarios/{request.user.id}_{project.name}ScenarioFile.h5"
-    )
-
-    response = FileResponse(
-        open(path, "rb"), as_attachment=True, filename=project_name + ".h5"
-    )
-
-    os.remove(path)
-
-    return response
-
-
-@login_required
-@require_POST
-def upload_preview(request, project_name):
-    """
-    Updates the preview of the project
-
-    Parameters
-    ----------
-    request : HttpRequest
-        The request the user send to get here.
-
-    Returns
-    -------
-    HttpResponse : status 200
-        On successfull POST request
-    HttpResponse : status 404
-        on all other occasions
-    """
-    project = get_object_or_404(Project, name=project_name, owner=request.user)
-    file = request.FILES["preview"]
-
-    project.preview.delete()
-    project.preview = file
-    project.save()
-
-    return HttpResponse(status=200)
+        return HttpResponse(status=200)
