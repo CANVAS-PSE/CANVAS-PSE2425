d0aa3b0f092b7ee878ab43b96f113f04258ebea7
plhrt <pl.hrtr@gmail.com>
2025-08-24 11:27:12 +0200

    upadted views on account management

--- canvas_editor/account_management/tests/test_urls.py
+++ canvas_editor/account_management/tests/test_urls.py
@@ -7,57 +7,29 @@ from account_management.views import (
     PasswordForgottenView,
     PasswordResetView,
     RegistrationView,
-    delete_account,
-    invalid_link,
-    logout_view,
-    update_account,
+    DeleteAccountView,
+    InvalidLinkView,
+    LogoutView,
+    UpdateAccountView,
 )
+
+URLS_TO_VIEWS = [
+    ("login", (), LoginView),
+    ("register", (), RegistrationView),
+    ("logout", (), LogoutView),
+    ("update_account", (), UpdateAccountView),
+    ("delete_account", (), DeleteAccountView),
+    ("password_reset", ("uidb64", "token"), PasswordResetView),
+    ("invalid_link", (), InvalidLinkView),
+    ("confirm_deletion", ("uidb64", "token"), ConfirmDeletionView),
+    ("password_forgotten", (), PasswordForgottenView),
+]
 
 
 class TestUrls(SimpleTestCase):
     """Test if the urls return the correct view."""
 
-    def test_login_url_resolves(self):
-        """Test login url."""
-        url = reverse("login")
-        self.assertEqual(resolve(url).func.view_class, LoginView)
-
-    def test_register_url_resolves(self):
-        """Test register url."""
-        url = reverse("register")
-        self.assertEqual(resolve(url).func.view_class, RegistrationView)
-
-    def test_logout_url_resolves(self):
-        """Test logout url."""
-        url = reverse("logout")
-        self.assertEqual(resolve(url).func, logout_view)
-
-    def test_update_account_url_resolves(self):
-        """Test update account url."""
-        url = reverse("update_account")
-        self.assertEqual(resolve(url).func, update_account)
-
-    def test_delete_account_url_resolves(self):
-        """Test delete account url."""
-        url = reverse("delete_account")
-        self.assertEqual(resolve(url).func, delete_account)
-
-    def test_password_reset_url_resolves(self):
-        """Test password reset url."""
-        url = reverse("password_reset", args=["uidb64", "token"])
-        self.assertEqual(resolve(url).func.view_class, PasswordResetView)
-
-    def test_invalid_link_url_resolves(self):
-        """Test the invalid link url."""
-        url = reverse("invalid_link")
-        self.assertEqual(resolve(url).func, invalid_link)
-
-    def test_confirm_deletion_url_resolves(self):
-        """Test the confirm deletion url."""
-        url = reverse("confirm_deletion", args=["uidb64", "token"])
-        self.assertEqual(resolve(url).func.view_class, ConfirmDeletionView)
-
-    def test_password_forgotten_url_resolves(self):
-        """Test the password forgotten url."""
-        url = reverse("password_forgotten")
-        self.assertEqual(resolve(url).func.view_class, PasswordForgottenView)
+    def test_urls_resolve_to_correct_views(self):
+        for name, args, view in URLS_TO_VIEWS:
+            url = reverse(name, args=args)
+            self.assertEqual(resolve(url).func.view_class, view)
--- canvas_editor/account_management/views.py
+++ canvas_editor/account_management/views.py
@@ -1,13 +1,12 @@
 from allauth.socialaccount.models import SocialAccount
 from django.contrib import messages
 from django.contrib.auth import get_user_model, login, logout, update_session_auth_hash
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from django.http import (
     HttpResponse,
-    HttpResponsePermanentRedirect,
     HttpResponseRedirect,
     JsonResponse,
 )
@@ -16,7 +15,7 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views import View
-from django.views.decorators.http import require_GET, require_POST
+from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 
 from canvas import message_dict, path_dict, view_name_dict
@@ -78,7 +77,7 @@ class RegistrationView(FormView):
 
     def form_invalid(self, form) -> HttpResponse:
         """Handle invalid form."""
-        return render(self.request, "account_management/register.html", {"form": form})
+        return render(self.request, self.template_name, {"form": form})
 
     def form_valid(self, form) -> HttpResponseRedirect:
         """Handle valid form."""
@@ -129,10 +128,11 @@ class ConfirmDeletionView(View):
         return redirect("login")
 
 
-class LoginView(View):
+class LoginView(FormView):
     """View that handles the login functionality."""
 
-    login_template = "account_management/login.html"
+    template_name = "account_management/login.html"
+    form_class = LoginForm
 
     def dispatch(self, request, *args, **kwargs):
         """Decide where to dispatch this request to.
@@ -143,124 +143,130 @@ class LoginView(View):
             return redirect(view_name_dict.projects_view)
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request) -> HttpResponse:
-        """Render the login page."""
-        return render(request, self.login_template, {"form": LoginForm()})
+    def get_success_url(self):
+        """Get the projects url at runtime."""
+        return reverse(view_name_dict.projects_view)
 
-    def post(self, request) -> HttpResponse:
-        """Log the user in."""
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            user.backend = "django.contrib.auth.backends.ModelBackend"
-            login(request, user)
-            return redirect(view_name_dict.projects_view)
-        return render(request, self.login_template, {"form": form})
+    def form_invalid(self, form):
+        """Handle invalid login form."""
+        return render(self.request, self.template_name, {"form": form})
 
-
-@require_POST
-def logout_view(request) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
-    """Log out the current user and redirect to the login page."""
-    logout(request)
-    return redirect(view_name_dict.login_view)
+    def form_valid(self, form):
+        """Handle valid login form."""
+        user = form.get_user()
+        user.backend = "django.contrib.auth.backends.ModelBackend"
+        login(self.request, user)
+        return super().form_valid(form)
 
 
-@require_POST
-@login_required
-def update_account(request) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
-    """Update the user's account information."""
-    user = request.user
+class LogoutView(View):
+    """Handle logout of the user."""
 
-    form = UpdateAccountForm(instance=user, data=request.POST, files=request.FILES)
+    def post(self, request):
+        """Log out the current user and redirect to the login page."""
+        logout(request)
+        return redirect(view_name_dict.login_view)
 
-    if form.is_valid():
-        user.first_name = form.cleaned_data["first_name"]
-        user.last_name = form.cleaned_data["last_name"]
-        user.email = form.cleaned_data["email"]
-        # Set the username to the email for consistency
-        user.username = user.email
 
-        old_password = form.cleaned_data["old_password"]
-        new_password = form.cleaned_data["new_password"]
+class UpdateAccountView(LoginRequiredMixin, View):
+    """Handle changes to the users account."""
 
-        if old_password and new_password:
-            user.set_password(new_password)
-            update_session_auth_hash(request, user)
-            send_password_change_email(user, request)
+    @staticmethod
+    def _update_profile_picture(
+        user: User, delete_picture: bool, new_profile_picture
+    ) -> None:
+        """Update the user's profile picture or delete it if requested."""
+        profile, _ = UserProfile.objects.get_or_create(user=user)
 
-        _update_profile_picture(
-            user=user,
-            delete_picture=request.POST.get("delete_picture") == "1",
-            new_profile_picture=form.cleaned_data["profile_picture"],
+        if delete_picture and profile.profile_picture:
+            profile.profile_picture.delete()
+            profile.profile_picture = path_dict.default_profil_pic
+        elif new_profile_picture:
+            profile.profile_picture.delete()
+            profile.profile_picture = new_profile_picture
+
+        user.save()
+        profile.save()
+
+    @staticmethod
+    def send_password_change_email(user, request) -> None:
+        """Send an email to the user to confirm that their password has been changed."""
+        subject = "Password Change Confirmation"
+
+        # Create the token for the user
+        uid = urlsafe_base64_encode(str(user.id).encode())
+        token = default_token_generator.make_token(user)
+
+        base_url = request.build_absolute_uri("/")
+        # Create the URL for the password change page
+        password_reset_url = f"{base_url}password_reset/{uid}/{token}/"
+
+        message = render_to_string(
+            "account_management/accounts/password_change_confirmation_email.html",
+            {
+                "user": user,
+                "password_reset_url": password_reset_url,
+            },
         )
 
-        messages.success(request, message_dict.account_created_text)
-    else:
-        for field in form:
-            for error in field.errors:
-                messages.error(request, f"Error in {field.label}: {error}")
-    return redirect(request.META.get("HTTP_REFERER", view_name_dict.projects_view))
+        to_email = user.email
+        email = EmailMessage(subject, message, to=[to_email])
+        email.send()
+
+    def post(self, request):
+        """Update the user's account information."""
+        user = request.user
+
+        form = UpdateAccountForm(instance=user, data=request.POST, files=request.FILES)
+
+        if form.is_valid():
+            user.first_name = form.cleaned_data["first_name"]
+            user.last_name = form.cleaned_data["last_name"]
+            user.email = form.cleaned_data["email"]
+            # Set the username to the email for consistency
+            user.username = user.email
+
+            old_password = form.cleaned_data["old_password"]
+            new_password = form.cleaned_data["new_password"]
+
+            if old_password and new_password:
+                user.set_password(new_password)
+                update_session_auth_hash(request, user)
+                self.send_password_change_email(user, request)
+
+            self._update_profile_picture(
+                user=user,
+                delete_picture=request.POST.get("delete_picture") == "1",
+                new_profile_picture=form.cleaned_data["profile_picture"],
+            )
+
+            messages.success(request, message_dict.account_created_text)
+        else:
+            for field in form:
+                for error in field.errors:
+                    messages.error(request, f"Error in {field.label}: {error}")
+        return redirect(request.META.get("HTTP_REFERER", view_name_dict.projects_view))
 
 
-def _update_profile_picture(
-    user: User, delete_picture: bool, new_profile_picture
-) -> None:
-    """
-    Update the user's profile picture or delete it if requested.
-    """
-    profile, _ = UserProfile.objects.get_or_create(user=user)
+class GetUserInfoView(LoginRequiredMixin, View):
+    """Allow to check if the user is an open id user."""
 
-    if delete_picture and profile.profile_picture:
-        profile.profile_picture.delete()
-        profile.profile_picture = path_dict.default_profil_pic
-    elif new_profile_picture:
-        profile.profile_picture.delete()
-        profile.profile_picture = new_profile_picture
-
-    user.save()
-    profile.save()
+    def get(self, request):
+        """Check if the user is logged in via OpenID and return the information as JSON."""
+        user = request.user
+        is_openid_user = SocialAccount.objects.filter(user=user).exists()
+        return JsonResponse({"is_openid_user": is_openid_user})
 
 
-@login_required
-@require_GET
-def get_user_info(request):
-    """
-    Check if the user is logged in via OpenID and return the information as JSON.
-    """
-    user = request.user
-    is_openid_user = SocialAccount.objects.filter(user=user).exists()
-    return JsonResponse({"is_openid_user": is_openid_user})
-
-
-def send_password_change_email(user, request) -> None:
-    """Send an email to the user to confirm that their password has been changed."""
-    subject = "Password Change Confirmation"
-
-    # Create the token for the user
-    uid = urlsafe_base64_encode(str(user.id).encode())
-    token = default_token_generator.make_token(user)
-
-    base_url = request.build_absolute_uri("/")
-    # Create the URL for the password change page
-    password_reset_url = f"{base_url}password_reset/{uid}/{token}/"
-
-    message = render_to_string(
-        "account_management/accounts/password_change_confirmation_email.html",
-        {
-            "user": user,
-            "password_reset_url": password_reset_url,
-        },
-    )
-
-    to_email = user.email
-    email = EmailMessage(subject, message, to=[to_email])
-    email.send()
-
-
-class PasswordResetView(View):
+class PasswordResetView(FormView):
     """View that handles the password reseting via the link send to the email address."""
 
-    password_reset_template = "account_management/password_reset.html"
+    template_name = "account_management/password_reset.html"
+    form_class = PasswordResetForm
+
+    def get_success_url(self):
+        """Get the url where the user gets redirected at runtime."""
+        return reverse(view_name_dict.login_view)
 
     def dispatch(self, request, uidb64, token):
         """Decide where to dispatch this request to.
@@ -278,96 +284,104 @@ class PasswordResetView(View):
 
         return super().dispatch(request)
 
-    def get(self, request) -> HttpResponse:
-        """Render the password reset form."""
-        return render(
-            request, self.password_reset_template, {"form": PasswordResetForm()}
+    def form_invalid(self, form):
+        """Handle invalid password reset form."""
+        return render(self.request, self.template_name, {"form": form})
+
+    def form_valid(self, form):
+        """Handle valid password reset form."""
+        self.user.set_password(form.cleaned_data["new_password"])
+        self.user.save()
+
+        # Logout from all sessions
+        logout(self.request)
+
+        return super().form_valid(form)
+
+
+class InvalidLinkView(TemplateView):
+    """Render a page indicating that the link is invalid."""
+
+    template_name = "account_management/invalid_link.html"
+
+
+class DeleteAccountView(LoginRequiredMixin, FormView):
+    """Handle account deletion."""
+
+    template_name = "project_management/projects.html"
+    form_class = DeleteAccountForm
+    http_method_names = ["post"]
+
+    def get_success_url(self):
+        """Get the url of the login page at runtime."""
+        return reverse(view_name_dict.login_view)
+
+    def get_form_kwargs(self):
+        """Pass the user to the form."""
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
+
+    def form_invalid(self, form):
+        """Handle invalid delete account form."""
+        for field in form:
+            for error in field.errors:
+                messages.error(self.request, f"Error in {field.label}: {error}")
+
+        return redirect(
+            self.request.META.get("HTTP_REFERER", view_name_dict.projects_view)
         )
 
-    def post(self, request) -> HttpResponse:
-        """Validate the reset from and reset the password."""
-        form = PasswordResetForm(request.POST)
-
-        if form.is_valid():
-            self.user.set_password(form.cleaned_data["new_password"])
-            self.user.save()
-
-            # Logout from all sessions
-            logout(request)
-
-            # Redirect to login page
-            return redirect("login")
-        else:
-            return render(request, self.password_reset_template, {"form": form})
+    def form_valid(self, form):
+        """Handle valid delete account form."""
+        self.request.user.delete()
+        logout(self.request)
+        return super().form_valid(form)
 
 
-@require_GET
-def invalid_link(request):
-    """
-    Render a page indicating that the link is invalid.
-    """
-    return render(request, "account_management/invalid_link.html")
-
-
-@require_POST
-@login_required
-def delete_account(request) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
-    """Delete the user's account."""
-    if request.method == "POST":
-        form = DeleteAccountForm(request.user, request.POST)
-        if form.is_valid():
-            request.user.delete()
-            logout(request)
-            return redirect(view_name_dict.login_view)
-        else:
-            for field in form:
-                for error in field.errors:
-                    messages.error(request, f"Error in {field.label}: {error}")
-
-    return redirect(request.META.get("HTTP_REFERER", view_name_dict.projects_view))
-
-
-class PasswordForgottenView(View):
+class PasswordForgottenView(FormView):
     """View if a user doesn't remember its password and wants to reset it."""
 
-    password_forgotten_template = "account_management/password_forgotten.html"
+    template_name = "account_management/password_forgotten.html"
+    form_class = PasswordForgottenForm
 
-    def get(self, request) -> HttpResponse:
-        """Render the password forgotten form."""
-        return render(
-            request, self.password_forgotten_template, {"form": PasswordForgottenForm()}
+    def get_success_url(self):
+        """Get the login view url at runtime."""
+        return reverse(view_name_dict.login_view)
+
+    @staticmethod
+    def send_password_forgotten_email(user, request):
+        """Send an email to the user to reset their password."""
+        subject = "Password Reset"
+
+        # Create the token for the user
+        uid = urlsafe_base64_encode(str(user.id).encode())
+        token = default_token_generator.make_token(user)
+
+        base_url = request.build_absolute_uri("/")
+        # Create the URL for the password change page
+        password_reset_url = f"{base_url}password_reset/{uid}/{token}/"
+
+        message = render_to_string(
+            "account_management/accounts/password_forgotten_email.html",
+            {
+                "user": user,
+                "password_reset_url": password_reset_url,
+            },
         )
 
-    def post(self, request) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
-        """Validate the form and send the reset email."""
-        form = PasswordForgottenForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data["email"]
-            user = User.objects.get(email=email)
-            send_password_forgotten_email(user, request)
-        return redirect("login")
+        to_email = user.email
+        email = EmailMessage(subject, message, to=[to_email])
+        email.send()
 
+    def form_invalid(self, form):
+        """Handle invalid password reset form."""
+        return render(self.request, self.template_name, {"form": form})
 
-def send_password_forgotten_email(user, request):
-    """Send an email to the user to reset their password."""
-    subject = "Password Reset"
+    def form_valid(self, form):
+        """Handle valid password reset form."""
+        email = form.cleaned_data["email"]
+        user = User.objects.get(email=email)
+        self.send_password_forgotten_email(user, self.request)
 
-    # Create the token for the user
-    uid = urlsafe_base64_encode(str(user.id).encode())
-    token = default_token_generator.make_token(user)
-
-    base_url = request.build_absolute_uri("/")
-    # Create the URL for the password change page
-    password_reset_url = f"{base_url}password_reset/{uid}/{token}/"
-
-    message = render_to_string(
-        "account_management/accounts/password_forgotten_email.html",
-        {
-            "user": user,
-            "password_reset_url": password_reset_url,
-        },
-    )
-
-    to_email = user.email
-    email = EmailMessage(subject, message, to=[to_email])
-    email.send()
+        return super().form_valid(form)
