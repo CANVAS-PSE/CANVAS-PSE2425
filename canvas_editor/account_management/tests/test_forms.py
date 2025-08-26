bf3b5ccfdd0ae161b873d4c37efe81079f301ae5
Lisann Engel <uumbm@student.kit.edu>
2025-08-26 10:55:47 +0200

    zwischenstand

--- canvas_editor/account_management/tests/test_forms.py
+++ canvas_editor/account_management/tests/test_forms.py
@@ -51,21 +51,33 @@ class FormTestMixin:
     default_data = {}
 
     def create_form(self, **overrides):
+        """
+        Create a form instance with the given overrides.
+        """
         data = self.default_data.copy()
         data.update(overrides)
         return self.form_class(data=data)
 
     def create_form_with_instance(self, **overrides):
+        """
+        Create a form instance with the given overrides while using the specified instance.
+        """
         data = self.default_data.copy()
         data.update(overrides)
         return self.form_class(instance=self.instance, data=data)
 
     def create_form_with_user(self, **overrides):
+        """
+        Create a form instance with the given overrides while using the specified user.
+        """
         data = self.default_data.copy()
         data.update(overrides)
         return self.form_class(user=self.user, data=data)
 
     def assertFormErrorMessage(self, form, field, expected_message):
+        """
+        Assert that the form contains the expected error message for the given field.
+        """
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors[field], [expected_message])
 
--- canvas_editor/account_management/tests/test_signals.py
+++ canvas_editor/account_management/tests/test_signals.py
@@ -2,32 +2,46 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from account_management.models import UserProfile
-from canvas.test_constants import TEST_PASSWORD
+from canvas.test_constants import (
+    TEST_PASSWORD,
+    TEST_EMAIL_2,
+    FIRST_NAME_TEST_ENVIRONMENT,
+    LAST_NAME_TEST_ENVIRONMENT,
+    PROFILE_PIC_PATH_TEST,
+)
 
 
 class TestSignals(TestCase):
-    """Contains test for the signals of the user profile model."""
+    """
+    Contains test for the signals of the user profile model.
+    """
 
     def setUp(self) -> None:
-        """Configure the testing environment for the tests."""
+        """
+        Configure the testing environment for the tests.
+        """
         self.user = User.objects.create_user(
-            username="test@mail.de",
-            first_name="first_name",
-            last_name="last_name",
-            email="test@mail.de",
+            username=TEST_EMAIL_2,
+            first_name=FIRST_NAME_TEST_ENVIRONMENT,
+            last_name=LAST_NAME_TEST_ENVIRONMENT,
+            email=TEST_EMAIL_2,
             password=TEST_PASSWORD,
         )
 
     def test_create_user_profile(self):
-        """Test the automatic generation of the user profile."""
+        """
+        Test the automatic generation of the user profile.
+        """
         self.assertTrue(UserProfile.objects.filter(user=self.user).exists())
         user_profile = UserProfile.objects.get(user=self.user)
         self.assertEqual(user_profile.user, self.user)
 
     def test_save_user_profile(self):
-        """Test the automatic saving of the profile when saving the user."""
+        """
+        Test the automatic saving of the profile when saving the user.
+        """
         user_profile = self.user.userprofile
-        user_profile.profile_picture = "users/1/custom.jpg"
+        user_profile.profile_picture = PROFILE_PIC_PATH_TEST
         self.user.save()
         updated_profile = UserProfile.objects.get(user=self.user)
-        self.assertEqual(updated_profile.profile_picture, "users/1/custom.jpg")
+        self.assertEqual(updated_profile.profile_picture, PROFILE_PIC_PATH_TEST)
