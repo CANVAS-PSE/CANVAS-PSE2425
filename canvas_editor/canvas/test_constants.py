1234c6867c1cd306f39098fec0c1e5c72abf0333
Lisann Engel <uumbm@student.kit.edu>
2025-09-09 14:50:23 +0200

    project managment done

--- canvas_editor/canvas/test_constants.py
+++ canvas_editor/canvas/test_constants.py
@@ -39,7 +39,9 @@ NEW_EMAIL = "new_test@mail.de"
 TEST_FIRST_NAME_2 = "test2_first_name"
 TEST_LAST_NAME_2 = "test2_last_name"
 
+TEST_PROJECT_DESCRIPTION = "Test project description"
 TEST_PROJECT_NAME = "test_project"
+TEST_PROJECT_NAME_2 = "test_project_2"
 TEST_PROFILE_PICTURE = "profile_picture.jpg"
 OLD_PROFILE_PICTURE_JPG = "old_profile_picture.jpg"
 PROFILE_PIC_PREFIX = "users"
@@ -63,11 +65,12 @@ OLD_PASSWORD_FIELD = "old_password"
 FIRST_NAME_FIELD = "first_name"
 LAST_NAME_FIELD = "last_name"
 EMPTY_FIELD = ""
+WHITESPACE_FIELD = " "
 
 # Constants for teseting project management features in a Canvas project
 
 HELIOSTAT_NAME = "Heliostat 1"
-LIGHT_SOURCE_NAME = "Light source 1"
+LIGHT_SOURCE_NAME = "Light Source"
 RECEIVER_NAME = "Receiver 1"
 NEW_HELIOSTAT_NAME = "New Heliostat"
 NEW_LIGHT_SOURCE_NAME = "New Light Source"
@@ -99,3 +102,58 @@ POSITION_COORDINATE_X = 0
 POSITION_COORDINATE_Y = 1
 POSITION_COORDINATE_Z = 2
 CURVATURE_DEFAULT = 0.0
+
+# Constants for project_managment tests
+USERNAME_PROJECT_PAGE_TEST = "testuser"
+TEST_HELIOSTAT_NAME = "Heliostat"
+TEST_LIGHT_SOURCE_NAME = "Light source"
+TEST_RECEIVER_NAME = "Receiver"
+SETTINGS_NAME = "Settings"
+UPDATED_DESCRIPTION = "Updated description"
+UPDATED_PROJECT_NAME = "Updated_project_name"
+TEST_PROJECT_DESCRIPTION_2 = "Another test project description"
+TEST_PROJECT_NAME_2 = "another_test_project"
+COPY_SUFFIX = "_copy"
+SHARED_SUFFIX = "_shared"
+OBJECTS_COUNT_TEST = 2
+PROJECT_NAME_PROJECT_PAGE_TEST = "Test_project"
+PROJECT_DESCRIPTION_PROJECT_PAGE_TEST = "Test project description"
+PROJECT_DESCRIPTION_PROJECT_PAGE_TEST_2 = "Test project description"
+PROJECT_NAME_PROJECT_PAGE_TEST_2 = "Test_project_2"
+PROJECT_NAME_WITH_WHITESPACE = " Test project 2 "
+PROJECT_DESCRIPTION_WITH_WHITESPACE = " Test project description "
+PROJECT_NAME_DUPLICATE_NAME = "Test project 2"
+
+# Heliosat default values
+HELIOSTAT_POSITION_X = 0
+HELIOSTAT_POSITION_Y = 0
+HELIOSTAT_POSITION_Z = 0
+
+# Receiver default values
+RECEIVER_POSITION_X = 0
+RECEIVER_POSITION_Y = 50
+RECEIVER_POSITION_Z = 0
+RECEIVER_NORMAL_X = 0
+RECEIVER_NORMAL_Y = 1
+RECEIVER_NORMAL_Z = 0
+RECEIVER_TYPE_PLANAR = "planar"
+RECEIVER_CURVATURE_E = 0
+RECEIVER_CURVATURE_U = 0
+RECEIVER_PLANE_E = 8.629666667
+RECEIVER_PLANE_U = 7.0
+RECEIVER_RESOLUTION_E = 256
+RECEIVER_RESOLUTION_U = 256
+
+# Light Source default values
+LIGHT_SOURCE_NUMBER_OF_RAYS = 100
+LIGHT_SOURCE_TYPE = "sun"
+LIGHT_SOURCE_DISTRIBUTION_TYPE = "normal"
+LIGHT_SOURCE_MEAN = 0
+LIGHT_SOURCE_COVARIANCE = 4.3681e-06
+
+# Field names
+NAME_FIELD = "name"
+DESCRIPTION_FIELD = "description"
+OWNER_FIELD = "owner"
+POSITION_X_FIELD = "position_x"
+FILE_FIELD = "file"
